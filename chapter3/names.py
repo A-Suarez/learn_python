eb8f093f6697f67bf584c3d33012b97c9d55f213
Alejandro Suarez <alesuarez1123@gmail.com>
2026-03-30 20:03:43 -0400

    3-1

---
+++ chapter3/names.py
@@ -0,0 +1,10 @@
+friends_names = ['Corinne', 'Eryk', 'Morandi', 'Brett']
+first_message = f'{friends_names[0].title()} is my girlfriend'
+second_message = f'{friends_names[1]} plays soccer in Dallas'
+third_message = f'{friends_names[2]} is from Long Island'
+fourth_message = f'{friends_names[-1]} is from the Jersey Shore'
+
+print(first_message)
+print(second_message)
+print(third_message)
+print(fourth_message)
