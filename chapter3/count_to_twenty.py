b8a77e51895c5d2e8a65245cb861d45100342d6b
Alejandro Suarez <alesuarez1123@gmail.com>
2026-04-12 20:36:36 -0400

    chapter 4 work

---
+++ chapter3/count_to_twenty.py
@@ -0,0 +1,24 @@
+# count_twenty = [num + 1 for num in range(0,20)]
+# for num in count_twenty:
+#     print(num)
+
+# count_million = [num + 1 for num in range(0,1_000_000)]
+# for num in count_million:
+#     print(num)
+
+# print(max(count_million))
+# print(min(count_million))
+# print(sum(count_million))
+
+# nums = [value for value in range(1,21,2)]
+# for num in nums:
+#     print(num)
+
+# multiples_of_threes = [3,6,9,12,15,18,21,24,27,30]
+# for num in multiples_of_threes:
+#     print(num)
+
+nums_cube = [num ** 3 for num in range(1,21)]
+
+for num in nums_cube:
+    print(num)
