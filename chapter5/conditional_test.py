27bdc73677115f155343d6e3f302760f74ebb5ce
Alejandro Suarez <alesuarez1123@gmail.com>
2026-04-19 17:24:59 -0400

    chapter 5 work

---
+++ chapter5/conditional_test.py
@@ -0,0 +1,18 @@
+# car = "tesla"
+# print("Is car == 'tesla'? I predict True")
+# print(car == 'tesla')
+# cars = ['tesla', 'bmw', 'mclaren', 'ferarri', 'honda']
+# car = 'toyota'
+# cars.append(car)
+
+# if car in cars:
+#     print(f'I found the car! {car.title()}')
+# else: 
+#     print(f'Sorry! Car is not in list')
+
+nums = [0,1,2,3,4,5,6,7,8,9]
+
+for num in nums:
+    if num > 4 or num < 5:
+        print(num) 
+
---
+++ chapter5/voting.py
@@ -0,0 +1,13 @@
+# age = 19
+# if age >= 18:
+#     print('Congrats! you are able to vote.')
+#     print('Have you registered to vote?')
+
+ages = [17, 21]
+
+for age in ages:
+    if age >= 18:
+        print('You are able to vote!')
+    else:
+        print('You are not able to vote!')
+
